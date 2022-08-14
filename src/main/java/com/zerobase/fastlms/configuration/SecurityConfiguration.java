eda4aec8899ce0c50d2664c6401af77992235dc6
Hyunebin <qortjr83@naver.com>
2022-08-14 22:55:54 +0900

    [feat] : 타임리프를 통한 layout분리 **** 추가한점 **** fragments 추가 레이아웃 정리

--- src/main/java/com/zerobase/fastlms/configuration/SecurityConfiguration.java
+++ src/main/java/com/zerobase/fastlms/configuration/SecurityConfiguration.java
@@ -1,8 +1,6 @@
 package com.zerobase.fastlms.configuration;
 
 import com.zerobase.fastlms.member.service.MemberService;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
